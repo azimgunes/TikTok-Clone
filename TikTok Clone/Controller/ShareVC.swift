f79bf2a2ad1ae03cb4ec85277e795739ee834ddf
azimgunes <azimgunesofficial@gmail.com>
2024-09-02 23:18:23 +0300

    Contsraints have been rearranged.

--- TikTok Clone/Controller/ShareVC.swift
+++ TikTok Clone/Controller/ShareVC.swift
@@ -10,10 +10,19 @@ import AVFoundation
 
 class ShareVC: UIViewController {
     
+    //MARK: Proporties
     
-    @IBOutlet weak var thumbImageView: UIImageView!
+    
     let originalVideoUrl: URL
     var selectedPhoto : UIImage?
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var selectLabel: UILabel!
+    
+    @IBOutlet weak var postBut: UIButton!
+    @IBOutlet weak var draftsBut: UIButton!
+    
     
     
     override func viewDidLoad() {
@@ -32,6 +41,27 @@ class ShareVC: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    @IBAction func allowToComments(_ sender: UISwitch) {
+    }
+    
+    
+    @IBAction func allawDeuts(_ sender: UISwitch) {
+    }
+    
+    @IBAction func allowAdding(_ sender: UISwitch) {
+    }
+    
+    
+    @IBAction func saveToDevice(_ sender: UISwitch) {
+    }
+    
+    @IBAction func postButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func draftsButton(_ sender: UIButton) {
     }
     
 }
