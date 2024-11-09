97b222c24b72ea68c12b88df08617047f2a55da5
azimgunes <azimgunesofficial@gmail.com>
2024-11-09 19:14:05 +0300

    ChatVC created, UITableView added to project for messaging.

---
+++ TikTok Clone/Cell/MessageCell.swift
@@ -0,0 +1,23 @@
+//
+//  MessageCell.swift
+//  TikTok Clone
+//
+//  Created by Azim Güneş on 9.11.2024.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TikTok Clone/Controller/ChatVC.swift
@@ -0,0 +1,29 @@
+//
+//  ChatVC.swift
+//  TikTok Clone
+//
+//  Created by Azim Güneş on 9.11.2024.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
